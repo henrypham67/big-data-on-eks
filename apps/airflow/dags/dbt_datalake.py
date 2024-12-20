ecfdf627ce04011dde6c65ab3be4cfa2dae4c1be
João Pedro da Costa Loureiro <joaopedrodacostaloureiro@Joaos-MacBook-Pro.local>
2024-12-20 14:23:29 -0300

    deploying the DBT project

---
+++ apps/airflow/dags/dbt_datalake.py
@@ -0,0 +1,29 @@
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
+import os
+from datetime import datetime
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="airflow_db",
+        profile_args={"schema": "public"},
+    ),
+)
+
+my_cosmos_dag = DbtDag(
+    project_config=ProjectConfig(
+        "/usr/local/airflow/dags/dbt_datalake",
+    ),
+    profile_config=profile_config,
+    execution_config=ExecutionConfig(
+        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+    ),
+    # normal dag parameters
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="my_cosmos_dag",
+    default_args={"retries": 2},
+)
